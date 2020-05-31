f9b0d9a2bde352bde508b5a3f82468b412cd14e4
ZechangSun <60177057+ZechangSun@users.noreply.github.com>
2020-05-31 22:56:39 +0800

    Update basic_query.py

--- covid_19_database/basic_query.py
+++ covid_19_database/basic_query.py
@@ -1,61 +1,124 @@
-#得到某个国家各个省市病例人数
-#sql语句部分定义函数date_num
-CREATE OR REPLACE FUNCTION date_num(DATE_ date)
-  RETURNS table(case_num bigint,
-			   city_name VARCHAR(20)) AS $$
-begin
-return QUERY SELECT count(case_id) as case_num,city
-From cases,SITE
-Where cases.site_id=SITE.site_id
-and situation='confirmed' and confirmed_date<DATE_ and country='china'
-Group by city;
-end;$$
-LANGUAGE 'plpgsql';
-#python批量生成sql语句
-import pandas as pd
-def get_date_list(begin_date,end_date):
-    date_list = [x.strftime('%Y-%m-%d') for x in list(pd.date_range(start=begin_date, end=end_date))]
-return date_list
-sql_file=open(r'C:\Users\26089\Desktop\update_file.txt','w',encoding='utf-8')#路径根据文件位置确定
-def wsql(begin_date,end_date):
-for i in get_date_list(begin_date,end_date):
-print(i)
-str_i=str(i)
-line="select * from date_num('%s');"%i
-    sql_file.write(line+'\n')
-wsql('DATE1','DATE2')
-sql_file.close()
+import MySQLdb
 
-#得到某个类型药物的研发情况
-#sql语句定义函数drug_num
-CREATE OR REPLACE FUNCTION drug_num(DATE_ date)
-  RETURNS table(insti_location VARCHAR(20),
-			   drug_count bigint) AS $$
-begin
-return QUERY With researcher_institution(researcher_id,trial_id,institution_id,institution_location)
-As (select researcher_id,trial_id,institution_id,institution_location
-From researcher_experiment natural join institution natural join institution_researcher natural join experiment
-Where start_date<=DATE_ and end_date>=DATE_),
-Drug_trial(trial_id,drug_id,drug_type)
-As (select trial_id,drug_id,drug_type
-From trial_drug natural join drug)
-Select institution_location,count(drug_id) as drug_count
-From researcher_institution natural join drug_trial
-Where drug_type='TYPE'
-group by institution_location;
-end;$$
-LANGUAGE 'plpgsql';
-#python批量生成sql语句
-import pandas as pd
-def get_date_list(begin_date,end_date):
-    date_list = [x.strftime('%Y-%m-%d') for x in list(pd.date_range(start=begin_date, end=end_date))]
-return date_list
-sql_file=open(r'C:\Users\26089\Desktop\update_file.txt','w',encoding='utf-8')#路径根据文件位置确定
-def wsql(begin_date,end_date):
-for i in get_date_list(begin_date,end_date):
-print(i)
-str_i=str(i)
-line="select * from drug_num('%s');"%i
-    sql_file.write(line+'\n')
-wsql('DATE1','DATE2')
-sql_file.close()
+
+def researcher_query(db, location):
+	cursor = db.cursor()
+	query = """Select researcher.first_name, researcher.last_name
+		   From researcher, institution
+		   Where researcher.institution_id = institution.institution_id and institution.location = "%s"
+		"""%location
+	cursor.execute(query)
+	cursor.close()
+	retrun cursor.fetchall()
+
+	
+def drug_query(db, name):
+	cursor = db.cursor()
+	query = """Select trial_id,trial_stage,trial_status
+		   From trial_drug natural join clinical_trial as drugntrial
+		   Where drug_id in(select drug_id
+		   From drug
+		   Where drug_name='%s')"""%name
+	cursor.execute(query)
+	cursor.close()
+	return cursor.fetchall()
+
+
+def age_query(db):
+	cursor = db.cursor()
+	query = """Select gender,avg(age)
+		   From cases
+		   Group by gender)"""
+	cursor.execute(query)
+	cursor.close()
+	return cursor.fetchall()
+
+
+
+def age_query(db):
+	cursor = db.cursor()
+	query = """Select gender,avg(age)
+		   From cases
+		   Group by gender)"""
+	cursor.execute(query)
+	cursor.close()
+	return cursor.fetchall()
+
+
+def case_query(db):
+	cursor = db.cursor()
+	query = """
+		with site_suspected(site_id,suspected_case)
+		as (select site_id,count(case_id)
+		From cases
+		where situation='suspected'
+		Group by site_id),
+		site_confirmed(site_id,confirmed_case)
+		As (select site_id,count(case_id)
+		From cases
+		where situation='confirmed'
+		Group by site_id),
+		site_dead(site_id,dead_case)
+		As (select site_id,count(case_id)
+		From cases
+			where situation='dead'
+		Group by site_id)
+		Select site_id,suspected_case,confirmed_case,dead_case
+		From site_suspected natural join site_confirmed natural join site_dead
+		"""
+	cursor.execute(query)
+	cursor.close()
+	return cursor.fetchall()
+
+
+def case_query(db):
+	cursor = db.cursor()
+	query = """
+		with site_suspected(site_id,suspected_case)
+		as (select site_id,count(case_id)
+		From cases
+		where situation='suspected'
+		Group by site_id),
+		site_confirmed(site_id,confirmed_case)
+		As (select site_id,count(case_id)
+		From cases
+		where situation='confirmed'
+		Group by site_id),
+		site_dead(site_id,dead_case)
+		As (select site_id,count(case_id)
+		From cases
+			where situation='dead'
+		Group by site_id)
+		Select site_id,suspected_case,confirmed_case,dead_case
+		From site_suspected natural join site_confirmed natural join site_dead
+		"""
+	cursor.execute(query)
+	cursor.close()
+	return cursor.fetchall()
+
+def volunteer_query(db):
+	cursor = db.cursor()
+	query = """
+		With trial(trial_id)
+		As(select trial_id
+		From experiment	
+		Where experiment.batch_id=1),
+		trialndrug(trial_id,drug_name)
+		As(select trial_id,drug_name
+		From trial_drug natural join drug
+		Where trial_id in 
+  		(select * from trial)
+  		),
+		trialnvolunteer(trial_id,volunteer_num)
+		As(select trial_id,count(volunteer_id)
+		From volunteer_experiment
+		Group by trial_id
+		Having trial_id in 
+  		(select * from trial)
+ 		 )
+		Select *
+		From trialndrug natural join trialnvolunteer
+		"""
+	cursor.execute(query)
+	cursor.close()
+	return cursor.fetchall()
